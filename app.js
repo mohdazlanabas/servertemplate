fa9ce5f857dfc8755a091eafcf3905e22fe4adb7
Azlan Abas <mohdazlan.abas@gmail.com>
2022-03-10 09:26:32 +0800

    update

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send('Hello World!'));
+app.get("/home", (req, res) => res.send('Home Page!'));
 
 app.listen(process.env.PORT || port, () => console.log(`Llistening at http://localhost:${port}`));
